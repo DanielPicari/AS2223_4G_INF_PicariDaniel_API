07192bd50b820e77771119c43b8fe4781fd440c3
daniel.picari@studenti.ittsrimini.edu.it <daniel.picari@studenti.ittsrimini.edu.it>
2023-03-16 08:29:56 +0100

    Get del numero pari o dispari

--- AS2223_4G_INF_PicariDaniel_API/Controllers/MathController.cs
+++ AS2223_4G_INF_PicariDaniel_API/Controllers/MathController.cs
@@ -2,11 +2,27 @@
 
 namespace AS2223_4G_INF_PicariDaniel_API.Controllers
 {
+    [ApiController]
+    [Produces("application/json")]
+    [Route("api/v1")]
     public class MathController : Controller
     {
-        public IActionResult Index()
+        [HttpGet("NumeroPariDispari")]
+        public JsonResult PariDispari(int numero)
         {
-            return View();
+            string risultato;
+            string status;
+            if(numero %  2  == 0)
+            {
+                status = "ok";
+                risultato = "il numero è pari";
+            }
+            else
+            {
+                status = "ko";
+                risultato = "il numero è dispari";
+            }
+            return Json(new {output=numero,risultato=risultato,status=status});
         }
     }
 }
